57fe19e2fd806efa9bc5bdbb5ed2704cbb5d1310
jongo-love <jongolove01@gmail.com>
2024-02-13 18:44:47 +0300

    ...

--- app.py
+++ app.py
@@ -7,5 +7,11 @@ app = Flask(__name__)
 def HOME():
     return render_template('home.html')
 
+@app.route('/about')
+def ABOUT():
+    return render_template('about.html')
+
+
+
 if __name__ == '__main__':
     app.run(debug=True)
