7473c55ba8452c4d6ffdf11df30cfecc149e36dc
jongo-love <jongolove01@gmail.com>
2024-02-15 16:38:47 +0300

    Setting up the Login page and the Registration page , User registration in the  database  Thursday 16:38 pm 2024

--- app.py
+++ app.py
@@ -1,17 +1,37 @@
 # LINE 1-10 I'M CREATING ROUTES FOR THE HOME PAGE.
-from flask import Flask, render_template
+import time
+from flask import Flask, redirect, render_template, url_for
 from flask_sqlalchemy import SQLAlchemy
+from flask_wtf import FlaskForm
+from wtforms import EmailField, StringField, PasswordField, SubmitField
+from wtforms.validators import DataRequired
+from werkzeug.security import generate_password_hash, check_password_hash
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///nielsenaccident.db'  # SQLite database file path
+app.config['SECRET_KEY'] = 'your_secret_key'
 db = SQLAlchemy(app)
 
-# Define the User model
+
+# Define the User model/ Creating the USER table in the database.
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True,)
     username = db.Column(db.String(20), unique=True, nullable=False)
     password = db.Column(db.String(60), nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
+
+class LoginForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    email = EmailField('Email', validators=[DataRequired()])
+    submit = SubmitField('Login')
+
+
+class RegistrationForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    email = EmailField('Email', validators=[DataRequired()])
+    submit = SubmitField('Register')
 
 #with app.app_context():
     #db.create_all()
@@ -30,6 +50,52 @@ def ABOUT():
 @app.route('/contact')
 def CONTACT():
     return render_template('contact.html')
+
+@app.route('/', methods=['GET', 'POST'])
+def LOGIN():
+    time.sleep(1.5)
+    form = LoginForm()
+    error = None
+    if form.validate_on_submit():
+        # You can handle the form data here
+        username = form.username.data
+        password = form.password.data
+        email = form.email.data
+        user = User.query.filter_by(username=username).first()
+
+        if user:
+            if check_password_hash(user.password, password)and user.email == email:
+                return redirect(url_for('HOME'))
+            else:
+                error = 'Incorrect password. Please try again.'
+        else:
+            error = 'Username not found. Please register.'
+    return render_template('login.html', form=form, error=error)
+
+@app.route('/register', methods=['GET', 'POST']) 
+def REGISTER():
+    time.sleep(1.5)
+    form = RegistrationForm()
+    error = None
+    if form.validate_on_submit():
+        # You can handle the form data here
+        username = form.username.data
+        password = form.password.data
+        email = form.email.data
+        existing_user = User.query.filter_by(username=username).first()
+
+        if existing_user:
+            error = 'Username already exists. Please choose a different one.'
+        else:
+            hashed_password = generate_password_hash(password)
+            new_user = User(username=username, password=hashed_password,email=email)
+            db.session.add(new_user)
+            db.session.commit()
+            return redirect(url_for('LOGIN'))
+    
+    return render_template('register.html', form=form, error=error)
+
+
 
 
 
